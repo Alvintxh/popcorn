5f8a28a592978335c2142cebb373e23f983d7315
XH_Tian <xh_tian@XH-TiandeMacBook-Air.local>
2026-01-25 21:09:00 +0800

    date 2026/01/25

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QSizePolicy
+from PySide6.QtWidgets import QGridLayout
 from PySide6.QtWidgets import QLabel
 from PySide6.QtCore import Qt
 #from PySide6.QtGui import *
@@ -21,8 +22,12 @@ class MainWindow(QMainWindow):
         central.setAttribute(Qt.WA_StyledBackground, True)
         self.setCentralWidget(central)
 
-        label = QLabel(central)
+        label = QLabel()
         label.setText("popcorn")
+        label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        layout = QGridLayout(central)
+        layout.addWidget(label)
 
 
 def load_qss(filename: str) -> str:
