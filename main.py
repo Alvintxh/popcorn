dc5ee856b66ba06e047bfa16caa8576477270d42
XH_Tian <xh_tian@XH-TiandeMacBook-Air.local>
2026-01-25 20:38:48 +0800

    date 2026/01/25
    add central widget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
+from PySide6.QtWidgets import QLabel
 #from PySide6.QtCore import *
 #from PySide6.QtGui import *
 
@@ -8,10 +9,24 @@ class MainWindow(QMainWindow):
 
     def __init__(self) -> None:
         super().__init__()
+        self.InitMainWindow()
+
+    def InitMainWindow(self):
+        self.setWindowTitle("Popcorn")
+        self.resize(500, 300)
+        central = QWidget()
+        self.setCentralWidget(central)
+
+        label = QLabel(central)
+        label.setText("popcorn")
 
 
-if __name__ == "__main__":
+def main():
     app = QApplication()
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
